7b5931bfb0aab81d1be4215171065af7b54fc7c3
unknown <elib3n@gmail.com>
2018-07-13 19:13:28 +0300

    Create User From with photo select

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\UsersRequest;
 use App\User;
+use App\Role;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+
 
 class AdminUsersController extends Controller
 {
@@ -27,7 +29,8 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
-        return view('admin.users.create');
+        $roles = Role::lists('name','id')->all();
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -36,9 +39,11 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        return $request->all();
+        User::create($request->all());
+        
+        return redirect('/admin/users');
     }
 
     /**
